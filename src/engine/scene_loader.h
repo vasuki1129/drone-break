41ee3d56040fb57aa458dc3acea925a2c095c788
Deepfrog <froggy@deepfrog.net>
2026-03-29 19:50:43 -0400

    scene loading basics

--- src/engine/scene_loader.h
+++ src/engine/scene_loader.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "scene.h"
 #include "errorcheck.h"
+#include <future>
+
+
 namespace engine {
 
 class SceneLoader {
 public:
-  Error<bool> CueScene(std::string path);
+  Error<bool> CueScene(std::string path); // async load scene from json dump to on deck scene, for streaming
+  Error<bool> LoadScene(std::string path); // sync load scene from json dump to current scene
 private:
   Scene* current_scene;
   Scene* scene_in_progress_load;
 };
-
 }
